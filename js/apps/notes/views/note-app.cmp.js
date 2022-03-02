554bfb4dad7f62376efc50dd42654615457c8b71
Itzik Vaknin <itzikv3@gmail.com>
2022-03-02 12:03:23 +0200

    skeleton commit

---
+++ js/apps/notes/views/note-app.cmp.js
@@ -0,0 +1,43 @@
+// import { bookService } from '../services/book-service.js'
+//import eventbus???
+// import bookFilter from '../cmps/book-filter.cmp.js';
+// import bookList from '../cmps/book-list.cmp.js';
+// import bookAdd from './book-add.cmp.js';
+
+
+/* <filter></filter>
+<note-list>
+    <note-preview>
+        <note-txt\img\video>
+    </note-preview>
+</note-list> */
+
+export default {
+    template: `
+        <section class="">
+        <!-- <filter></filter>
+        <add button>
+        <note-list>
+        </note-list>  -->
+        </section>
+    `,
+    components: {
+        // bookFilter,
+        // bookList,
+        // bookAdd
+    },
+    data() {
+        return {
+
+        };
+    },
+    created() {
+
+    },
+    methods: {
+
+    },
+    computed: {
+
+    },
+};
---
+++ js/router.js
@@ -0,0 +1,40 @@
+// import appsusApp from './views/app-sus.cmp.js';
+import homePage from './views/home-page.cmp.js'
+import mailApp from './apps/mail/views/app-smail.cmp.js'
+import notesApp from './apps/notes/views/note-app.cmp.js'
+// import bookApp from './views/book-app.cmp.js';
+// import aboutPage from './views/about-page.cmp.js'
+// import bookDetails from './views/book-details.cmp.js';
+// import bookAdd from './views/book-add.cmp.js';
+// import carEdit from './views/car-edit.cmp.js';
+
+const routes = [{
+        path: '/',
+        component: homePage
+    },
+    {
+        path: '/appsus/notes',
+        component: notesApp
+    },
+    // {
+    //     path: '/appsus/miss/add',
+    //     component: missAdd
+    // },
+    {
+        path: '/appsus/mail',
+        component: mailApp
+    },
+    // {
+    //     path: '/appsus/smail/add',
+    //     component: missAdd
+    // },
+    // {
+    //     path: '/car/edit/:carId?',
+    //     component: carEdit
+    // },
+];
+
+export const router = VueRouter.createRouter({
+    routes,
+    history: VueRouter.createWebHashHistory()
+});
